11f85845dd5e8ae80e691c495a0ac2cc330eb378
Argeu Souza <argeusousaptbr@gmail.com>
2026-05-12 11:27:31 -0300

    feat: update to Medico

--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import med.voll.api.endereco.Endereco;
 
 @Entity(name = "Paciente")
